0bbf454178d800fe869499224b7ac18798e25bb4
ygtorkc <109255432+ygtorkc@users.noreply.github.com>
2023-02-11 23:18:50 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+        public static int fib(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        public static void main(String[] args) {
+            int n = 10;
+            for (int i = 0; i < n; i++) {
+                System.out.print(fib(i) + " ");
+            }
+        }
+    }
+
